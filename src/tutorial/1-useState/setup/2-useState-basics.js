d0a4c34989ef53265f76172eed782a923ca75b0f
Ali Yanni <aliryanni@gmail.com>
2022-12-08 18:30:51 -0500

    updated application

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("Hello World"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState("Random Title");
+
+  const handleClick = () => {
+    if (text === "Random Title") {
+      setText("Hello World");
+    } else {
+      setText("Random Title");
+    }
+  };
+
+  return (
+    <>
+      <h1>{text}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // const [person, setPerson] = useState({
+  //   name: "Peter",
+  //   age: 24,
+  //   message: "Random Message",
+  // });
+
+  const [name, setName] = useState("Peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("Random Message");
+
+  const changeMessage = () => {
+    //setPerson({ ...person, message: "Hello World" });
+    setMessage("Hello World");
+  };
+
+  return (
+    <>
+      {/* <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3> */}
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button type="button" className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
